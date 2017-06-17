255589a46fcd0b13d2b74b9ebf2292bab540bd9b
winsipho <winston_ds@hotmail.com>
2017-06-17 00:14:43 -0600

    agregando etiquetas

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <form>
-<input type="text" name="nombre_de_usuario">
-<input type="text" name="contraseña">
-<input type="text" name="correo">
+<p>Nombre de usuario <input type="text" name="nombre_de_usuario"></p>
+<p>Contraseña <input type="text" name="contraseña"></p>
+</p>correo <input type="text" name="correo"></p>
 </form>
