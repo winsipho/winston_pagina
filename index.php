bccc5acd06520e689c8c243ad5928964cef7abd7
winsipho <winston_ds@hotmail.com>
2017-06-17 00:17:39 -0600

    boton enviar con submit

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 <p>Nombre de usuario <input type="text" name="nombre_de_usuario"></p>
 <p>Contraseña <input type="text" name="contraseña"></p>
 </p>correo <input type="text" name="correo"></p>
+<input type="submit" value="enviar">
 </form>
