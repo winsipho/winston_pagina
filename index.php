00306e4916b753f9aab829439e831bd5cd7b0130
winsipho <winston_ds@hotmail.com>
2017-06-23 12:42:11 -0600

    correcciones

--- index.php
+++ index.php
@@ -41,33 +41,34 @@
     </li>
   </ul>
   </nav>
+
+
+  <center>
+  <form method="post" action="registro_manejo.php">
+  <p>Usuario <input  required type="text" name="nombre_de_usuario"></p>
+  <p>Contraseña <input required type="password" name="contraseña"></p>
+  </p>correo <input required type="text" name="correo"></p>
+  </p>Telefono <input required type="text" name="telefono" onkeypress="return valida(event)"></p>
+
+  <script>
+  function valida(e){
+      tecla = (document.all) ? e.keyCode : e.which;
+      //captar que el usuario presiona una tecla onkeypress="return valida(event) linea 89
+      //Tecla de retroceso para borrar, siempre la permite
+      if (tecla==8){
+          return true;
+      }
+
+      // Patron de entrada, en este caso solo acepta numeros
+      patron =/[0-9]/;
+      tecla_final = String.fromCharCode(tecla);
+      return patron.test(tecla_final);
+  }
+  </script>
+  </p>Nacionalidad <input required type="text" name="nacionalidad"></p>
+  <input type="submit" value="enviar">
+  </form>
+  </center>
 </div>
-<center>
-<form method="post" action="registro_manejo.php">
-<p>Usuario <input  required type="text" name="nombre_de_usuario"></p>
-<p>Contraseña <input required type="password" name="contraseña"></p>
-</p>correo <input required type="text" name="correo"></p>
-</p>Telefono <input required type="text" name="telefono" onkeypress="return valida(event)"></p>
-
-<script>
-function valida(e){
-    tecla = (document.all) ? e.keyCode : e.which;
-    //captar que el usuario presiona una tecla onkeypress="return valida(event) linea 89
-    //Tecla de retroceso para borrar, siempre la permite
-    if (tecla==8){
-        return true;
-    }
-
-    // Patron de entrada, en este caso solo acepta numeros
-    patron =/[0-9]/;
-    tecla_final = String.fromCharCode(tecla);
-    return patron.test(tecla_final);
-}
-</script>
-</p>Nacionalidad <input required type="text" name="nacionalidad"></p>
-<input type="submit" value="enviar">
-</form>
-</center>
-
 </body>
 </html>
