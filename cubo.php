ecb4d4015e1a9c09b6a4d053b50de2bd2f747e4d
winsipho <winston_ds@hotmail.com>
2017-08-16 16:02:31 -0600

    prueba2

--- cubo.php
+++ cubo.php
@@ -2,16 +2,16 @@
 <head>
 </head>
 <body>
-  <form id="form1" onsubmit="return false">
+  <form id="formulario1" onsubmit="return false">
     <input type="number" id="numero1" name="numero1" value="0">
     <p id="resultado"> </p>
-    <button id="idbutton" >resultado</button>
+    <button id="idboton" >resultado</button>
   </form>
 
 </body>
 <noscript>tu navegador no soporta JS. <noscript>
 <script>
-var formulario=document.getElementById("form1");
+var formulario=document.getElementById("formulario1");
 document.getElementById("idboton").addEventListener("click", function(){
   var numero = document.getElementById("numero1").value;
   var cubo(numer*numero*numero);
