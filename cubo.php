bf53185300639e969ee4cb2a2481ba7954c9e58e
winsipho <winston_ds@hotmail.com>
2017-08-17 11:30:28 -0600

    pequeñños detalles

--- cubo.php
+++ cubo.php
@@ -9,12 +9,12 @@
   </form>
 
 </body>
-<noscript>tu navegador no soporta JS. <noscript>
+<noscript>tu navegador no soporta JS. </noscript>
 <script>
 var formulario=document.getElementById("formulario1");
 document.getElementById("idboton").addEventListener("click", function(){
   var numero = document.getElementById("numero1").value;
-  var cubo(numero*numero*numero);
+  var cubo=(numero*numero*numero);
   document.getElementById("resultado").innerHTML = cubo;
 });
 </script>
